e7112a70bf6c0c0d31d53fbcf411e35bebdac3c6
Joao Paulo <joaobarbosasm@gmail.com>
2023-03-22 10:23:45 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: Text('O Idolo do Brasil'),
+        backgroundColor: Colors.red[900],
+      ),
+      body: Center(
+                ),
+      ),
+    ),
+  );
+}
